8e497b3fdca75642484253d693180fa880c15e09
Tufis-Lastoveschi Stefan <70372191+TufisStefan@users.noreply.github.com>
2021-03-14 12:26:03 +0200

    Delete Java directory

--- Java/src/main/java/repository/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import domain.Reservation;
-
-public interface ReservationRepository extends CrudRepository<Long, Reservation>{
-
-}
--- Java/src/main/java/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import domain.User;
-
-public interface UserRepository extends CrudRepository<Long, User>{
-
-    void login(String username, String password);
-}
